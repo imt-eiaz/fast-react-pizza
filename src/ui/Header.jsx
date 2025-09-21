1ce84d4b01ee880a851c98d694dce4f7d6382dba
imt-eiaz <imteiaz@gmail.com>
2025-09-21 21:46:38 +0100

    6. Building the App Layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Co. </Link>
+      <p>Imtiaz</p>
+    </header>
+  );
+}
+
+export default Header;
