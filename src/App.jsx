239218abaf8e64315e0ea4fedc8e9257e792b6cf
imt-eiaz <imteiaz@gmail.com>
2025-09-22 08:48:39 +0100

    Error in  Menu when loading menu in Loader function

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ const router = createBrowserRouter([
       {
         path: "/menu",
         element: <Menu />,
+        // loader: menuLoader,
       },
       {
         path: "/cart",
--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,17 @@
+// import { useLoaderData } from "react-router-dom";
+// import { loader } from "./menuLoader";
+import { getMenu } from "../../services/apiRestaurant";
+
 function Menu() {
+  // const menu = useLoaderData();
+  // console.log(menu);
   return <h1>Menu</h1>;
 }
+
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
+}
+loader();
 
 export default Menu;
